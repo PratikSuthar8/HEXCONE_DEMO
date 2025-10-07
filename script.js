a477ae710e36dfd4f55b81ffa7fc7687d9060ffc
Janick <136822147+PratikSuthar8@users.noreply.github.com>
2025-10-07 23:18:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Hamburger toggle
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+
+hamburger.addEventListener("click", () => {
+	hamburger.classList.toggle("active");
+	navLinks.classList.toggle("active");
+});
